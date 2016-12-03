a3278a52b061f63f2ccf38958a2b50d89600511e
MarshallGarey <gaomarshall9@gmail.com>
2016-12-03 01:02:13 -0700

    moving to fragments

---
+++ app/src/main/java/com/example/mgarey2/familymap/model/Event.java
@@ -0,0 +1,20 @@
+package com.example.mgarey2.familymap.model;
+
+/**
+ * Created by Marshall on 12/2/2016.
+ * Storage for information about an event in a person's life.
+ * All data in JSON is required - there are no optional fields.
+ */
+public class Event {
+    private String eventId;
+    private String personId;
+    private String latitude;
+    private String longitutde;
+    private String country;
+    private String city;
+    private String description;
+    private String year;
+    private String descendant;
+
+
+}
---
+++ app/src/main/java/com/example/mgarey2/familymap/model/Person.java
@@ -0,0 +1,23 @@
+package com.example.mgarey2.familymap.model;
+
+/**
+ * Created by Marshall on 12/2/2016.
+ * * Storage for information about a person.
+ */
+public class Person {
+
+    // Required data:
+
+    private String descendant;
+    private String personId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String spouse;
+
+    // Optional data:
+    private String father;
+    private String mother;
+
+
+}
